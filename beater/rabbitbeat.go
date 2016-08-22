6001bf6664076acd4f5aa708d40940b7d2c08eb0
Kevin Retzke <kretzke@fnal.gov>
2016-08-22 18:04:02 -0500

    basic working beat

--- beater/rabbitbeat.go
+++ beater/rabbitbeat.go
@@ -1,7 +1,10 @@
 package beater
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"text/template"
 	"time"
 
 	"github.com/elastic/beats/libbeat/beat"
@@ -9,13 +12,25 @@ import (
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/libbeat/publisher"
 
+	"github.com/streadway/amqp"
+
 	"github.com/retzkek/rabbitbeat/config"
 )
 
+const (
+	RetryFallback = 10 * time.Second
+)
+
 type Rabbitbeat struct {
-	done   chan struct{}
-	config config.Config
-	client publisher.Client
+	done         chan struct{}
+	config       config.Config
+	client       publisher.Client
+	connection   *amqp.Connection
+	connClosing  chan *amqp.Error
+	connBlocking chan amqp.Blocking
+	channel      *amqp.Channel
+	chanClosing  chan *amqp.Error
+	inbox        <-chan amqp.Delivery
 }
 
 // Creates beater
@@ -26,9 +41,18 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	}
 
 	bt := &Rabbitbeat{
-		done: make(chan struct{}),
+		done:   make(chan struct{}),
 		config: config,
 	}
+
+	logp.Info("establishing AMQP connection")
+	if err := bt.setupAMQP(); err != nil {
+		logp.WTF("%s", err)
+	}
+	if err := bt.setupConsumer(); err != nil {
+		logp.WTF("%s", err)
+	}
+
 	return bt, nil
 }
 
@@ -36,27 +60,154 @@ func (bt *Rabbitbeat) Run(b *beat.Beat) error {
 	logp.Info("rabbitbeat is running! Hit CTRL-C to stop it.")
 
 	bt.client = b.Publisher.Connect()
-	ticker := time.NewTicker(bt.config.Period)
-	counter := 1
 	for {
 		select {
 		case <-bt.done:
 			return nil
-		case <-ticker.C:
+		case c := <-bt.connClosing:
+			logp.Warn("AMQP connection closed (%s: %s)", c.Code, c.Reason)
+			bt.setupAMQP()
+		case c := <-bt.connBlocking:
+			if c.Active {
+				logp.Warn("AMQP connection blocked (%s)", c.Reason)
+			} else {
+				logp.Info("AMQP connection unblocked")
+			}
+		case c := <-bt.chanClosing:
+			logp.Warn("AMQP channel closed (%s: %s)", c.Code, c.Reason)
+			bt.setupConsumer()
+		case r := <-bt.inbox:
+			logp.Info("Event received")
+			event := common.MapStr{
+				"@timestamp": common.Time(time.Now()),
+				"type":       b.Name,
+			}
+			if err := json.Unmarshal(r.Body, &event); err != nil {
+				r.Nack(false, false)
+				logp.Err("error unmarshalling message (%s)", err)
+			} else {
+				bt.client.PublishEvent(event)
+				r.Ack(false)
+				logp.Info("Event sent")
+			}
 		}
-
-		event := common.MapStr{
-			"@timestamp": common.Time(time.Now()),
-			"type":       b.Name,
-			"counter":    counter,
-		}
-		bt.client.PublishEvent(event)
-		logp.Info("Event sent")
-		counter++
 	}
 }
 
 func (bt *Rabbitbeat) Stop() {
 	bt.client.Close()
 	close(bt.done)
+}
+
+// amqpURI generates the connection URI from the config parameters.
+// If scrubbed is true, the password field is set to "xxx"
+func (bt *Rabbitbeat) amqpURI(scrubbed bool) string {
+	conf := bt.config
+	if scrubbed {
+		conf.Password = "xxx"
+	}
+	tpl := template.Must(template.New("uri").Parse(conf.URITemplate))
+	var b bytes.Buffer
+	if err := tpl.Execute(&b, conf); err != nil {
+		logp.WTF("error generating URI from template \"%s\" (%s)", conf.URITemplate, err)
+	}
+	return b.String()
+}
+
+// retryDuration parses the Retry config setting duration. In case of error the
+// RetryFallback duration is returned.
+func (bt *Rabbitbeat) retryDuration() time.Duration {
+	retry, err := time.ParseDuration(bt.config.Retry)
+	if err != nil {
+		logp.Warn("error parsing Retry duration, using \"%s\" (%s)", RetryFallback.String(), err)
+		return RetryFallback
+	}
+	return retry
+}
+
+// setupAMQP initializes the connection to the AMQP broker
+func (bt *Rabbitbeat) setupAMQP() error {
+	if bt.connection != nil {
+		bt.connection.Close()
+	}
+
+	logp.Info("connecting to AMQP broker at %s", bt.amqpURI(true))
+	var err error
+	connect := func() error {
+		bt.connection, err = amqp.Dial(bt.amqpURI(false))
+		return err
+	}
+	for err = connect(); err != nil; err = connect() {
+		logp.Err("error connecting to AMQP broker. retry in %s (%s)", bt.config.Retry, err)
+		time.Sleep(bt.retryDuration())
+	}
+
+	logp.Info("connection established to AMQP broker")
+
+	// listen for close and blocking events
+	bt.connClosing = bt.connection.NotifyClose(make(chan *amqp.Error))
+	bt.connBlocking = bt.connection.NotifyBlocked(make(chan amqp.Blocking))
+
+	return nil
+}
+
+// setupConsumer sets up the AMQP channel and initializes the consumer.
+func (bt *Rabbitbeat) setupConsumer() error {
+	if bt.connection == nil {
+		return fmt.Errorf("AMQP connection not open")
+	}
+
+	// setup channel
+	var err error
+	bt.channel, err = bt.connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	// setup exchange and queue
+	if err := bt.channel.ExchangeDeclare(
+		bt.config.Exchange,
+		bt.config.ExchangeType,
+		bt.config.Durable,
+		bt.config.AutoDeleteExchange,
+		false, //internal
+		false, //noWait
+		nil,   //args
+	); err != nil {
+		return fmt.Errorf("error declaring exchange (%s)", err)
+	}
+	if _, err := bt.channel.QueueDeclare(
+		bt.config.Queue,
+		bt.config.Durable,
+		bt.config.AutoDeleteQueue,
+		bt.config.Exclusive,
+		false, //noWait
+		nil,   //args
+	); err != nil {
+		return fmt.Errorf("error declaring queue (%s)", err)
+	}
+	if err := bt.channel.QueueBind(
+		bt.config.Queue,
+		bt.config.RoutingKey,
+		bt.config.Exchange,
+		false, //noWait
+		nil,   //args
+	); err != nil {
+		return fmt.Errorf("error binding queue (%s)", err)
+	}
+
+	// start consumer
+	if bt.inbox, err = bt.channel.Consume(
+		bt.config.Queue,
+		bt.config.ConsumerTag,
+		false, //autoAck
+		bt.config.Exclusive,
+		true,  //noLocal
+		false, //noWait
+		nil,   //args
+	); err != nil {
+		return fmt.Errorf("error starting consumer (%s)", err)
+	}
+
+	return nil
 }
--- config/config.go
+++ config/config.go
@@ -3,12 +3,40 @@
 
 package config
 
-import "time"
-
 type Config struct {
-	Period time.Duration `config:"period"`
+	User               string `config:"user"`
+	Password           string `config:"password"`
+	Host               string `config:"host"`
+	VHost              string `config:"vhost"`
+	Port               string `config:"port"`
+	URITemplate        string `config:"uri_template"`
+	Retry              string `config:"retry"`
+	Exchange           string `config:"exchange"`
+	ExchangeType       string `config:"exchange_type"`
+	Queue              string `config:"queue"`
+	RoutingKey         string `config:"routing_key"`
+	Exclusive          bool   `config:"exclusive"`
+	Durable            bool   `config:"durable"`
+	AutoDeleteExchange bool   `config:"auto_delete_exchange"`
+	AutoDeleteQueue    bool   `config:"auto_delete_queue"`
+	ConsumerTag        string `config:"consumer_tag"`
 }
 
 var DefaultConfig = Config{
-	Period: 1 * time.Second,
+	User:               "guest",
+	Password:           "guest",
+	Host:               "localhost",
+	VHost:              "",
+	Port:               "5672",
+	URITemplate:        "amqp://{{.User}}:{{.Password}}@{{.Host}}:{{.Port}}/{{.VHost}}",
+	Retry:              "10s",
+	Exchange:           "amqp.fanout",
+	ExchangeType:       "fanout",
+	Queue:              "rabbitbeat",
+	RoutingKey:         "#",
+	Exclusive:          false,
+	Durable:            true,
+	AutoDeleteExchange: false,
+	AutoDeleteQueue:    false,
+	ConsumerTag:        "",
 }
